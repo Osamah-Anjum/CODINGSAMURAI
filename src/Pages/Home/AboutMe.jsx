271961e3a0ccb2e77ba37c8e5d7490903b3a941f
Osamah-Anjum <osamahbinjawaidalanjum@gmail.com>
2023-12-12 10:13:44 +0530

    commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section--box" >
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills--section--heading">About Me</h1>
+                    <p className="hero--section-description"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit culpa reiciendis odio cum, consectetur veritatis officia possimus optio laboriosam illo similique error, dolore assumenda sunt deserunt natus qui corporis debitis!</p>
+                    <p className="hero--section-description"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit culpa reiciendis odio cum, consectetur veritatis officia possimus optio laboriosam illo similique error?</p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,11 +1,17 @@
+import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
+import Testimonials from "../Testimonials";
 
 export default function Home () {
     return (
         <>
             <HeroSection />
             <MySkills />
+            <AboutMe />
+            <MyPortfolio />
+            <Testimonials />
         </>
     )
 }
